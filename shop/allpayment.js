6694053017ac1af454752960bc7f11c384541a51
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-11-10 09:32:51 +0800

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,27 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY TAKANASHI RIKKA
 
 *さ BANK*
 1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
+	ㅁ Comming soon
+		• waiting!!
 		
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ Coming soon
+		• waiting!!
 2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ Coming soon
+		• waiting!!
 3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ Coming soon
+		• waiting!!
 
 *さ PULSA*
 1. SMARTFREN
-	ㅁ 0887 4350 47326
+	ㅁ Coming soon
 2. TELKOMSEL
-	ㅁ 0821 9311 5723
+	ㅁ 6285751657758
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
